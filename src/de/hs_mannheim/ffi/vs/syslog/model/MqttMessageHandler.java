e235223899d4f21616ded9187414a26869f96e3a
plush2408 <david2408@web.de~>
2023-05-26 00:58:04 +0200

    now its your turn

--- src/de/hs_mannheim/ffi/vs/syslog/model/MqttMessageHandler.java
+++
@@ -1,7 +0,0 @@
-package de.hs_mannheim.ffi.vs.syslog.model;
-
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
-public interface MqttMessageHandler {
-    void handleMessage(String topic, MqttMessage message);
-}
